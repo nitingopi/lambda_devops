5187b5543d932f47f45253323d163500a677e7da
Nitin Gopi <28586@TELXSIDS.NET>
2022-02-07 14:14:12 +0530

    added function body

--- lambda_function.py
+++ lambda_function.py
@@ -4,4 +4,11 @@ def lambda_handler(event, context):
     # d = {'col1':[1,2], 'col2':[3,4]}
     # df = pd.DataFrame(data=d)
     # print(df)
-    print('Done x1')
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": "Hello Lambda!" 
+        }
+    
