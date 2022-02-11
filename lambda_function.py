91a68e252cf36936ee83e68312dca1a4f5fb0afe
Nitin Gopi <28586@TELXSIDS.NET>
2022-02-11 15:52:58 +0530

    updated code

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,7 @@
 # import pandas as pd
-import requests, json
+import requests
+import json
+
 
 def lambda_handler(event, context):
     startDate = None
@@ -19,10 +21,12 @@ def lambda_handler(event, context):
         reportName = str(event['REPORT_NAME'])
 
     if 'DIMENSION' in event:
-        print('DIMENSION = ' + str(event['DIMENSION'])) # no information how to use
+        # no information how to use
+        print('DIMENSION = ' + str(event['DIMENSION']))
 
-    if 'METRICS' in event:    
-        print('METRICS = ' + str(event['METRICS'])) # no information how to use
+    if 'METRICS' in event:
+        # no information how to use
+        print('METRICS = ' + str(event['METRICS']))
 
     if 'TOKEN' in event:
         print('TOKEN = ' + str(event['TOKEN']))
@@ -38,9 +42,6 @@ def lambda_handler(event, context):
         URL1_params = "https://mediahub.invidi.it/api/impressions/v1/totals/campaigns"
 
         URL1 = f'{URL1_params}?startDate={startDate}&endDate={endDate}'
-               
-
-            
 
     # call API1 and get the list of campaign_id
     r1 = requests.get(url=URL1, headers=auth_token_header)
@@ -48,55 +49,62 @@ def lambda_handler(event, context):
     # extracting data in json format
     data1 = r1.json()
 
-
-
     # return final_data
     final_response = []
 
     for item in data1:
         if item["id"] and item["id"] != None:
-            pay_load = {"id":item["id"]}
+            pay_load = {"id": item["id"]}
             # call API2 corresponding to the campaign_id
-
-            res = requests.get(URL2, params=pay_load, headers=auth_token_header)
-            data2 = res.json() 
-            # return data2   
-            metrics = item["metrics"]
-            metrics_keys = metrics.keys()
-            # Merge both data
+            res = requests.get(URL2, params=pay_load,
+                               headers=auth_token_header)
+            data2 = res.json()
             campaigns = data2['campaigns']
-            for day in metrics_keys:
+            if startDate is not None and endDate is not None:
+                raw_impressions = item['metrics']['rawImpressions']
+                validated_impressions = item['metrics']['validatedImpressions']
                 for campaign in campaigns:
-                    # print(campaign)
-                    # return campaign
-                    if campaign['id'] == item['id']:
-                        data_set = {
-                            "ID": item["id"],
-                            "name": campaign["name"],
-                            "contentProvider": campaign["contentProvider"],
-                            "status": campaign["status"],
-                            "type": campaign["type"],
-                            "startTime": campaign["startTime"],
-                            "endTime": campaign["endTime"],
-                            "priority": campaign["priority"],
-                            # "notes": campaign["notes"],
-                            "advertiser": campaign["advertiser"],
-                            # "addExec": campaign["addExec"],
-                            "date": day,
-                            "raw_impressions": metrics[day]["rawImpressions"],
-                            "validated_impressions": metrics[day][
-                                "validatedImpressions"
-                            ],
-                        }
+                    data_set = merge_func(
+                        item, campaign, raw_impressions, validated_impressions)
                 final_response.append(data_set)
-
-    # return str(final_response)
+            else:
+                metrics = item["metrics"]
+                for day, impressions in metrics.items():
+                    raw_impressions = impressions['rawImpressions']
+                    validated_impressions = impressions['validatedImpressions']
+                    for campaign in campaigns:
+                        data_set = merge_func(
+                            item, campaign, raw_impressions, validated_impressions, day)
+                    final_response.append(data_set)
 
     return {
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json"
         },
-        "body": str(final_response) 
+        "body": str(final_response)
+    }
+
+
+def merge_func(item, campaign,  raw_impressions, validated_impressions, day=None):
+    if campaign['id'] == item['id']:
+        data_set = {
+            "ID": item["id"],
+            "name": campaign["name"],
+            "contentProvider": campaign["contentProvider"],
+            "status": campaign["status"],
+            "type": campaign["type"],
+            "startTime": campaign["startTime"],
+            "endTime": campaign["endTime"],
+            "priority": campaign["priority"],
+            # "notes": campaign["notes"],
+            "advertiser": campaign["advertiser"],
+            # "addExec": campaign["addExec"],
+            "date": day,
+            "raw_impressions": raw_impressions,
+            "validated_impressions": validated_impressions,
         }
-    
+        if day is None:
+            data_set.pop("date")
+
+    return data_set
